4d1c851f5007aab7f345e2824c530e8408a4efdb
lanceycruz <lancey@hypefactor.ai>
2024-05-07 14:09:22 +0800

    background change

--- pong/main.lua
+++ pong/main.lua
@@ -40,8 +40,8 @@ require 'Paddle'
 require 'Ball'
 
 -- size of our actual window
-WINDOW_WIDTH = 1280
-WINDOW_HEIGHT = 720
+WINDOW_WIDTH = 1500
+WINDOW_HEIGHT = 1000
 
 -- size we're trying to emulate with push
 VIRTUAL_WIDTH = 432
